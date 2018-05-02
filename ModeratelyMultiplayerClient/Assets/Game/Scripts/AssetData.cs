848b28f05eb1e79620dc4dca770e0c55cb781a88
clarabie <falagard@gmail.com>
2018-05-01 22:00:52 -0400

    Re-arrange SQLite folder. Add AssetData scriptable object, and menu item for generating asset database with placeholder code.

---
+++ ModeratelyMultiplayerClient/Assets/Game/Scripts/AssetData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "AssetData", menuName = "ModeratelyMultiplayer/AssetData", order = 1)]
+public class AssetData : ScriptableObject
+{
+    public string Id = System.Guid.NewGuid().ToString();
+    public Collider Collider;
+    public GameObject ViewPrefab;
+}
---
+++ ModeratelyMultiplayerClient/Assets/Game/Scripts/Editor/EditorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace MMF
+{
+    public class EditorHelper
+    {
+        public static T[] GetAtPath<T>(string path)
+        {
+            ArrayList al = new ArrayList();
+            string[] fileEntries = Directory.GetFiles(Application.dataPath + "/" + path);
+
+            foreach (string fileName in fileEntries)
+            {
+                int assetPathIndex = fileName.IndexOf("Assets");
+                string localPath = fileName.Substring(assetPathIndex);
+
+                Object t = AssetDatabase.LoadAssetAtPath(localPath, typeof(T));
+
+                if (t != null)
+                    al.Add(t);
+            }
+            T[] result = new T[al.Count];
+            for (int i = 0; i < al.Count; i++)
+                result[i] = (T)al[i];
+
+            return result;
+        }
+    }
+}
