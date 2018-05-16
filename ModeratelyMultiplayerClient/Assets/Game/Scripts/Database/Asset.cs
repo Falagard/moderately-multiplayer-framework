9fdfe08f292eb3cc997bb8bc427e43cdff8eb62e
clarabie <falagard@gmail.com>
2018-05-15 22:32:53 -0400

    Add LiteDB - alternative to SQLite.

--- ModeratelyMultiplayerClient/Assets/Game/Scripts/Database/Asset.cs
+++ ModeratelyMultiplayerClient/Assets/Game/Scripts/Database/Asset.cs
@@ -1,10 +1,12 @@
-﻿using SQLite4Unity3d;
+﻿using LiteDB;
+using SQLite4Unity3d;
 
 namespace MMF
 {
     public class Asset
     {
         [PrimaryKey]
+        [LiteDB.BsonId]
         public string Id { get; set; }
 
         public string Name { get; set; }
--- ModeratelyMultiplayerClient/Assets/Game/Scripts/Editor/GenerateAssetDatabaseEditor.cs
+++ ModeratelyMultiplayerClient/Assets/Game/Scripts/Editor/GenerateAssetDatabaseEditor.cs
@@ -2,7 +2,9 @@
 using UnityEditor;
 using System.Collections;
 using System;
+using System.Net.NetworkInformation;
 using MMF;
+using LiteDB;
 
 public class GenerateAssetDatabaseEditor : EditorWindow
 {
@@ -12,9 +14,35 @@ public class GenerateAssetDatabaseEditor : EditorWindow
         var ds = new DataService("game.db");
         ds.CreateDb();
 
-        //Debug.Log("Got here");
+        var connectionString = string.Format(@"Assets/StreamingAssets/{0}", "lite.db");
         var assetDatas = EditorHelper.GetAtPath<AssetDataScriptableObject>("Game/Data");
 
+        try
+        {
+            using (var database = new LiteDatabase(connectionString))
+            {
+                database.Shrink();
+                
+                var collection = database.GetCollection<Asset>();
+
+                foreach (var assetData in assetDatas)
+                {
+                    var asset = new Asset()
+                    {
+                        Name = assetData.name,
+                        Id = assetData.Id
+                    };
+
+                    collection.Insert(asset);
+                }
+
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+        
         foreach (var assetData in assetDatas)
         {
             //Debug.Log(assetData.name + " " + assetData.Id);
